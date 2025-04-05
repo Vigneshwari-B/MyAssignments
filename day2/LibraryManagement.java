f53a8d0177ff583b005a7559e558e582d68db902
Vigneshwari-B <vidyabalakrish1427@gmail.com>
2025-04-05 20:39:40 +0530

    Add files via upload

---
+++ day2/LibraryManagement.java
@@ -0,0 +1,13 @@
+package week2.day2;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		
+		Library lM=new Library();
+		lM.addBook("Murder Mystry");
+		lM.issueBook();
+
+	}
+
+}
