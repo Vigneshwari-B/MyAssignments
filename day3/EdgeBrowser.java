4ebdec77744792a45c04a718cfc71cb75e4c0ca0
Vigneshwari-B <vidyabalakrish1427@gmail.com>
2025-04-05 20:45:55 +0530

    Add files via upload

---
+++ day3/EdgeBrowser.java
@@ -0,0 +1,13 @@
+package week2.day3;
+
+public class EdgeBrowser {
+	
+	public static void main(String[] args) {
+		
+		Browser b= new Browser();
+		b.launchBrowser("Chrome");
+		b.loadUrl();
+	}
+
+
+}
---
+++ day3/Library.java
@@ -0,0 +1,25 @@
+package week2.day3;
+
+public class Library {
+	
+	public String addBook(String bookTittle) {
+		
+		System.out.println(bookTittle);
+		System.out.println("Book added successfully");
+		return bookTittle;
+	}
+
+	public void issueBook() {
+	
+		System.out.println("Book issued successfully");
+	}
+	
+	public static void main(String[] args) {
+		
+		Library L=new Library();
+		L.addBook("Murder Mystry");
+		L.issueBook();
+
+	}
+
+}
